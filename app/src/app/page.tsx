be63f18863649091e9428db2025f738cbaa7c150
CSCairney <charlescairney@yahoo.co.uk>
2024-09-07 18:00:01 +0100

    temp changes

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,9 +4,29 @@ import {PageHeader} from "@/components/common/PageHeader";
 import {WelcomeHero} from "@/components/landing/WelcomeHero";
 import {MatchRecentScores} from "@/components/sports/components/MatchRecentScores";
 import {useAppSelector} from "@/app/store";
+import {useAccounts} from "@/api/accounts/useAccounts";
+import {useDispatch} from "react-redux";
+import {setAvailableAccounts} from "@/redux/stores/session";
+import {useEffect} from "react";
 
 
 export default function Home() {
+    const { getAllAccounts } = useAccounts();
+    const dispatch = useDispatch();
+
+    const fetchAccounts = async () => {
+        try {
+            const accounts = await getAllAccounts();
+            dispatch(setAvailableAccounts(accounts));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchAccounts();
+    }, []);
+
     return (
         <>
             <main className={styles.main}>
--- app/src/components/sports/components/MatchRecentScores/index.tsx
+++ app/src/components/sports/components/MatchRecentScores/index.tsx
@@ -1,11 +1,12 @@
 import { useMatches } from "@/api/sports/useMatches";
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { MatchResponseDTO } from "@/types/sports/match";
 import { MatchQueryParams } from "@/types/matches";
 import styles from './styles.module.scss';
 import Loader from "@/components/common/Loader";
 import { toast } from "react-toastify";
-import {useAppSelector} from "@/app/store";
+import {RootState, useAppSelector} from "@/app/store";
+import {retrieveAccountName} from "@/helpers/accounts";
 
 export type MatchRecentScoresProps = {
     amount: number;
@@ -18,9 +19,9 @@ export type MatchRecentScoresProps = {
 export const MatchRecentScores = (
     { amount, sportId, sliding = false, variant = 'primary', className }: MatchRecentScoresProps
 ) => {
+    const accounts = useAppSelector((state) => state.session.availableAccounts);
     const [matches, setMatches] = useState<MatchResponseDTO[]>([]);
     const { getAllMatches, loading } = useMatches();
-    const slidingBarRef = useRef<HTMLDivElement>(null);
     const params: MatchQueryParams = { amount, sportId };
 
     const retrieveRecentMatches = useCallback(async () => {
@@ -42,12 +43,14 @@ export const MatchRecentScores = (
         return <Loader />;
     }
 
+    const duplicatedMatches = matches.concat(matches);
+
     return (
         <div className={`${styles.sliderContainer} ${styles[variant]} ${className}`}>
-            <div className={`${styles.slidingBar} ${sliding ? styles.sliding : ''}`} ref={slidingBarRef}>
-                {matches.map(match => (
-                    <div key={match.id} className={styles.matchItem}>
-                        <p>{match.scorePlayerOne} - {match.scorePlayerTwo}</p>
+            <div className={`${styles.slidingBar} ${sliding ? styles.sliding : ''}`}>
+                {duplicatedMatches.map((match, index) => (
+                    <div key={`${match.id}-${index}`} className={styles.matchItem}>
+                        <p>{retrieveAccountName(match.playerOneId, accounts)} {match.scorePlayerOne} - {match.scorePlayerTwo} {retrieveAccountName(match.playerTwoId, accounts)}</p>
                     </div>
                 ))}
             </div>
